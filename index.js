28e090b55502192f61fa3b3b2095eb0db6789629
Matt Ellis <m.t.ellis@gmail.com>
2014-10-23 12:10:44 +0100

    Support for intro pages
    A list at the start of the SUMMARY.md which doesn’t have a h2 will be
    treated as intro pages, and displayed directly under the Introduction
    node of the compiled GtBook

--- index.js
+++ index.js
@@ -12,11 +12,20 @@ module.exports = {
             // If the file contains an h2, we need to reformat it
             if (summary.content.match(/^## /m)) {
 
-                // Indent all existing list items, then replace the h2 with a top level list item
-                // and then remove any blank lines
-                summary.content = summary.content.replace(/^(\s*\* .*)$/gm, "    $1")
-                    .replace(/^## (.*)$/gm, "* $1")
-                    .replace(/(\*.*)\n\n/gm, "$1\n");
+                var parts = summary.content.split(/(?=##)/);
+                for (var i = 0; i < parts.length; i++) {
+                    // Remove any blank lines
+                    parts[i] = parts[i].replace(/(\*.*)\n\n/gm, "$1\n");
+
+                    // If the part has a heading, replace it and indent the child items in the list
+                    // Mark a heading as a part by smuggling some text into the list item.
+                    // Messy, but it's the only way.
+                    if (parts[i].indexOf('##') == 0) {
+                        parts[i] = parts[i].replace(/^([ \t]*\* .+)$/gm, "    $1")
+                            .replace(/^## (.*)$/gm, "* -XPARTX-$1")
+                    }
+                }
+                summary.content = parts.join('');
             }
 
             return summary;
@@ -30,16 +39,31 @@ module.exports = {
             // Replace top level li.chapter with li.part
             $('ul.summary > li.chapter').each(function(i, elem) {
                 var li = $(elem);
-                li.removeClass('chapter');
-                li.addClass('part');
 
-                if (i > 0)
-                    li.before('<li class="part divider" />');
+                // Replace the classes if the chapter is actually a part, and add a divider
+                if (li.text().indexOf('-XPARTX-') > 0) {
+                    li.removeClass('chapter');
+                    li.addClass('part');
+
+                    // Don't add a divider before the first part, let it come straight after the Introduction page
+                    if (i > 1)
+                        li.before('<li class="part divider" />');
+                }
             });
+
+            // remove chapter numbers from any intro pages (and child pages)
+            $('ul.summary > li.chapter span > b').remove();
+            $('ul.summary > li.chapter a > b').remove();
 
             // Remove the chapter number from the part header
             $('ul.summary > li.part > span > b').remove();
             $('ul.summary > li.part > a > b').remove();
+
+            // Replace the nasty munging of the name for part heading
+            $('ul.summary > li.part > span').each(function(i, elem) {
+                var span = $(elem);
+                span.text(span.text().replace(/-XPARTX-/, ""));
+            });
 
             // Bump the remaining chapter numbers so each chapter is only unique
             // within the part
